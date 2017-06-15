0bae9762d307466d8932e65e8a3ade26d1cd9cbc
pramodpix <pixpk333@gmail.com>
2017-06-15 15:52:08 +0530

    Create codeToChangeFileNames.py
    Flexibility to add custom filename and extension

--- codeToChangeFileNames.py
+++ codeToChangeFileNames.py
@@ -1,12 +1,14 @@
 import os
-def makeFolder(filepath):
+def changeFileName(filepath):
         directory=os.path.abspath(filepath)
         number=0
+        C_name='Img' #Change according to your requirement
+        ext='jpg' #Change according to your requirement
         for file_name in os.listdir(directory):
                 print (file_name)                
                 new_file_number = number + 1
  
-                new_file_name = '%s/%s%d.%s' % (directory,'Img' , new_file_number,'nef')
+                new_file_name = '%s/%s%d.%s' % (directory,C_name, new_file_number,ext)
                 old_file_name = '%s/%s' % (directory, file_name)
  
                 os.rename(old_file_name, new_file_name)
